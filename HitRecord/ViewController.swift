ae501e159561e4ec0e15128c0deb426bc09423f2
Noah Teshu <nteshu@gmail.com>
2015-05-03 15:37:35 -0400

    first commit

--- HitRecord/ViewController.swift
+++ HitRecord/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    var audioRecorder : AVAudioRecorder?
+    var audioPlayer : AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let dirPaths =
+        NSSearchPathForDirectoriesInDomains(.DocumentDirectory,
+            .UserDomainMask, true)
+        let docsDir = dirPaths[0] as! String
+        let soundFilePath =
+        docsDir.stringByAppendingPathComponent("sound.caf")
+        let soundFileURL = NSURL(fileURLWithPath: soundFilePath)
+        let recordSettings =
+        [AVEncoderAudioQualityKey: AVAudioQuality.Min.rawValue,
+            AVEncoderBitRateKey: 16,
+            AVNumberOfChannelsKey: 2,
+            AVSampleRateKey: 44100.0]
+        
+        var error: NSError?
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord,
+            error: &error)
+        
+        if let err = error {
+            println("audioSession error: \(err.localizedDescription)")
+        }
+        
+        audioRecorder = AVAudioRecorder(URL: soundFileURL,
+            settings: recordSettings as [NSObject : AnyObject], error: &error)
+        
+        if let err = error {
+            println("audioSession error: \(err.localizedDescription)")
+        } else {
+            audioRecorder?.prepareToRecord()
+        }
+
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +57,44 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        if audioRecorder?.recording == false {
+            //playButton.enabled = false
+            //stopButton.enabled = true
+            audioRecorder?.record()
+        }
+    }
+    
+    @IBAction func stopAudio(sender: AnyObject) {
+        //stopButton.enabled = false
+        //playButton.enabled = true
+        //recordButton.enabled = true
+        
+        if audioRecorder?.recording == true {
+            audioRecorder?.stop()
+        } else {
+          //  audioPlayer?.stop()
+        }
+    }
+    
+    @IBAction func playAudio(sender: AnyObject) {
+        if audioRecorder?.recording == false {
+            //stopButton.enabled = true
+            //recordButton.enabled = false
+            
+            var error: NSError?
+            
+            audioPlayer = AVAudioPlayer(contentsOfURL: audioRecorder?.url,
+                error: &error)
+            
+            audioPlayer?.delegate = self
+            
+            if let err = error {
+                println("audioPlayer error: \(err.localizedDescription)")
+            } else {
+                audioPlayer?.play()
+            }
+        }
+    }
 }
 
